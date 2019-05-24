a2ca17b4fff483f5c76b9e08fff2f62631374903
NG <nigeiselh@gmail.com>
2019-05-24 21:11:00 +0200

    init

---
+++ check-availability.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+LOGFILE=downtimes.log
+CSV_HEADERS="date,checked_servers,cmd,status"
+
+if [ ! -f "$LOGFILE" ]; then
+  touch "$LOGFILE"
+  echo -e "$CSV_HEADERS\n" >> "$LOGFILE"
+fi
+
+servers=('taobao.com' 'google.com' 'facebook.com' 'yahoo.com' 'amazon.de' '8.8.8.8' '1.1.1.1')
+
+for server in "${servers[@]}"; do
+  ping -c 1 $server;
+  if [[ "$?" == "0" ]]; then
+    echo "Internet Connection is available: $server is accessible";
+    exit 0;
+  else
+    echo "$server not accesible";
+  fi
+done
+
+echo "Internet Connection down, none of (${servers[*]}) are accessible"
+echo -e "$(date +%s), ${servers[*]}, ping -c 1, DOWN\n" >> "$LOGFILE"
+
+exit 1
---
+++ loop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+  echo "Press [CTRL+C] to stop.."
+  ./check-availability.sh
+  sleep 60
+done
